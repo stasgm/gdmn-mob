36c5902672f4a1a42be2d63b6d9d14c0bee908da
Stanislau Shliakhtsich <stasgm@gmail.com>
2021-03-23 18:39:39 +0300

    merge

--- apps/app/src/screens/HomeScreen.tsx
+++ apps/app/src/screens/HomeScreen.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { message } from '@lib/mylib';
 import { requests } from '@lib/client-api';
 import { Button } from 'react-native-paper';
-// import { MainContainer } from '@lib/common-ui/primitives/AppView';
-//import { CoolButton } from '@lib/common-ui';
-
-/* interface ChatListProps {
-  path?: string;
-}
- */
-/* const ChatList = (_props: ChatListProps) => {
-  return (
-    <MainContainer>
-      <Heading1>ChatList</Heading1>
-    </MainContainer>
-  );
-}; */
-
-console.log('Home');
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{message('you', 'hello')}</Text>
       <Text style={styles.title}>Задачи на сегодня:</Text>
       <Text style={styles.text}>На сегодня задач нет</Text>
       <Button onPress={() => console.log(requests.auth.login({ userName: 'Inna', password: '123' }))}>
--- packages/mylib/src/messages.ts
+++
@@ -1,7 +0,0 @@
-export const hello = (name: string) => {
-  return `Hello ${name}`;
-};
-
-export const goodbye = (name: string): string => {
-  return `Goodbye, ${name}`;
-};
