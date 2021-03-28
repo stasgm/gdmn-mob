60798d90b8ee39b94e86705b5bb6229bdae55c22
damned_god <stasgm@gmail.com>
2021-03-28 15:02:49 +0300

    feat: add logger and reactotron

--- packages/store/src/store/store.ts
+++ packages/store/src/store/store.ts
@@ -1,10 +1,12 @@
-import { combineReducers, createStore, Reducer, Store, applyMiddleware } from 'redux';
+import { combineReducers, createStore, Reducer, Store, applyMiddleware, compose, StoreEnhancer } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './rootReducer';
 import { IAuthState } from './auth/types';
+import reactotron from './ReactotronConfig';
 
 export { RootState } from './rootReducer';
 
@@ -22,8 +24,14 @@ function createReducer(asyncReducers: { [key: string]: Reducer } = {}) {
   });
 }
 
+const enhancer = reactotron.createEnhancer?.(true) as StoreEnhancer<any, any>;
+
+const composed = __DEV__
+  ? compose(enhancer, composeWithDevTools(applyMiddleware(thunk, logger)))
+  : applyMiddleware(thunk);
+
 function configureStore() {
-  const rootStore: Store = createStore(combineReducers(rootReducer), composeWithDevTools(applyMiddleware(thunk)));
+  const rootStore: Store = createStore(combineReducers(rootReducer), composed);
 
   const store: StoreWithAsyncReducers = {
     ...rootStore,
