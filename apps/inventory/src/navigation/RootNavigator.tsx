6c12513543020ec98a3560221e00324b4e028c9d
Elena Buraya <lenalatkovich@gmail.com>
2021-04-23 17:33:40 +0300

    feat: new app (inventory)

---
+++ apps/inventory/src/navigation/RootNavigator.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { DrawerNavigator } from "@lib/mobile-navigation";
+
+import { INavItem } from "@lib/mobile-navigation/src/navigation/DrawerNavigator";
+
+import MapScreen from "../screens/Maps";
+
+import DashboardNavigator from "./Root/DashboardNavigator";
+import DocumentsNavigator from "./Root/DocumentsNavigator";
+
+console.log("RootNavigator");
+
+const navItems: INavItem[] = [
+  {
+    name: "Dashboard",
+    title: "Дашборд",
+    icon: "view-dashboard-outline",
+    component: DashboardNavigator,
+  },
+  {
+    name: "Documents",
+    title: "Документы",
+    icon: "file-document-outline",
+    component: DocumentsNavigator,
+  },
+  {
+    name: "Map",
+    title: "Карта",
+    icon: "map-outline",
+    component: MapScreen,
+  },
+];
+
+const RootNavigator = () => {
+  return <DrawerNavigator items={navItems} />;
+};
+
+export default RootNavigator;
