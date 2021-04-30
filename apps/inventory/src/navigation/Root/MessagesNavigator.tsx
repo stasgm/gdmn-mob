3723e5ff6047b3b9de8df42ac7ace2449d087ed7
Elena Buraya <lenalatkovich@gmail.com>
2021-04-30 17:52:26 +0300

    chore: eslint

--- apps/inventory/src/navigation/Root/MessagesNavigator.tsx
+++ apps/inventory/src/navigation/Root/MessagesNavigator.tsx
@@ -1,7 +1,7 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 
-import MessagesScreen from "../../screens/MessagesScreen";
+import MessagesScreen from '../../screens/MessagesScreen';
 
 type MessagesStackParamList = {
   Messages: undefined;
@@ -11,10 +11,7 @@ const Stack = createStackNavigator<MessagesStackParamList>();
 
 const MessagesNavigator = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="Messages"
-      screenOptions={{ headerShown: false }}
-    >
+    <Stack.Navigator initialRouteName="Messages" screenOptions={{ headerShown: false }}>
       <Stack.Screen key="Messages" name="Messages" component={MessagesScreen} />
     </Stack.Navigator>
   );
