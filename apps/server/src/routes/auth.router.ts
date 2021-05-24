aecbb5c19feddde53be13a4da3ea68ce0455729e
sunnycreature <sunnycreature@gmail.com>
2021-05-24 10:09:57 +0300

    chore: misc

--- apps/server/src/routes/auth.router.ts
+++ apps/server/src/routes/auth.router.ts
@@ -1,5 +1,5 @@
 // import Router from 'koa-router';
-import compose from 'koa-compose';
+// import compose from 'koa-compose';
 import Router from 'koa-joi-router';
 
 import { signUp, logIn, logOut, getCurrentUser, getActivationCode, verifyCode } from '../controllers/auth';
@@ -17,7 +17,7 @@ router.prefix('/auth');
 router.post('/signup', authValidation.signup, signUp);
 router.post('/login', authValidation.login, logIn);
 router.post('/logout', authMiddleware, logOut);
-router.get('/user', compose([authMiddleware, deviceMiddleware]), getCurrentUser);
+router.get('/user', [authMiddleware, deviceMiddleware], getCurrentUser);
 router.get('/device/:deviceId/code', authValidation.getActivationCode, getActivationCode);
 router.post('/device/code', authValidation.verifyCode, verifyCode);
 
