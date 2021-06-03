c5302617886570f799b564fa513ff0fa30b00a53
Elena Buraya <lenalatkovich@gmail.com>
2021-06-03 20:28:49 +0300

    feat: screens for view visit

--- apps/app-trade-agents/src/screens/Routes/RouteDetailScreen.tsx
+++ apps/app-trade-agents/src/screens/Routes/RouteDetailScreen.tsx
@@ -1,11 +1,13 @@
 import { SubTitle } from '@lib/mobile-ui/src/components';
 import { docSelectors } from '@lib/store';
 import { RouteProp, useRoute } from '@react-navigation/native';
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Divider } from 'react-native-paper';
+import React, { useState } from 'react';
+import { ActivityIndicator, Text, View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
-import { globalStyles as styles } from '@lib/mobile-ui';
+import * as Location from 'expo-location';
+
+import { globalStyles, globalStyles as styles } from '@lib/mobile-ui';
 
 import { INamedEntity, IReference } from '@lib/types';
 
@@ -13,9 +15,22 @@ import { RoutesStackParamList } from '../../navigation/Root/types';
 import { IContact, IDebt, IOutlet, IRouteDocument } from '../../store/docs/types';
 
 import { contactRefMock, outletRefMock } from '../../store/docs/mock';
+import { useDispatch, useSelector } from '../../store';
+
+import { visitActions } from '../../store/visits/actions';
+import { ICoords } from '../../store/visits/types';
+
+import Visit from './components/Visit';
 
 const RouteDetailScreen = () => {
   const { routeId, id } = useRoute<RouteProp<RoutesStackParamList, 'RouteDetails'>>().params;
+  const { list: visits } = useSelector((state) => state.visits);
+
+  const dispatch = useDispatch();
+  //const ref = useRef<FlatList<IRouteLine>>(null);
+  //useScrollToTop(ref);
+
+  const [process, setProcess] = useState(false);
 
   const point = (docSelectors.selectByDocType('route') as IRouteDocument[])
     ?.find((e) => e.id === routeId)
@@ -45,38 +60,116 @@ const RouteDetailScreen = () => {
     saldoDebt: 0,
   };
 
-  return (
-    <View>
-      <SubTitle style={styles.title}>{point.outlet.name}</SubTitle>
-      <Divider />
-      <View>
-        {outlet && (
-          <>
-            <Text>{outlet.address}</Text>
-            <Text>{outlet.phoneNumber}</Text>
-          </>
-        )}
-      </View>
-      <Divider />
-      <SubTitle style={styles.title}>{`Договор №${contact?.contractNumber} от ${contact?.contractDate}`}</SubTitle>
-      <Divider />
-      <View>
-        {contact && (
-          <>
-            <Text>{contact.paycond}</Text>
-            <Text>{`Задолженность ${debt.saldo}`}</Text>
-          </>
-        )}
-      </View>
+  const handleNewVisit = async () => {
+    setProcess(true);
 
-      {/*<View style={styles.item}>
-        <View style={styles.details}>
-          <Text style={styles.name}>{point.outlet.name}</Text>
-          <Text style={[styles.number, styles.field]}>{point.ordNumber}</Text>
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      setProcess(false);
+      return;
+    }
+
+    const coords = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.BestForNavigation });
+    const date = new Date().toISOString();
+
+    dispatch(
+      visitActions.addOne({
+        id: `${routeId}${date}`,
+        routeLineId: Number(routeId),
+        dateBegin: date,
+        beginGeoPoint: coords as unknown as ICoords,
+        takenType: 'ONPLACE',
+      }),
+    );
+
+    setProcess(false);
+  };
+
+  //const renderItem = ({ item }: { item: IVisit }) => <VisitItem key={item.id} item={item} />;
+
+  return (
+    <View style={[styles.container, currStyles.content]}>
+      <View style={[styles.flexDirectionRow, currStyles.box]}>
+        <View style={currStyles.label} />
+        <View style={currStyles.info}>
+          <SubTitle style={[styles.title]}>{point.outlet.name}</SubTitle>
+          {outlet && (
+            <>
+              <Text>{outlet.address}</Text>
+              <Text>{`+375 ${outlet.phoneNumber}`}</Text>
+            </>
+          )}
         </View>
-      </View>*/}
+      </View>
+      <View style={[styles.flexDirectionRow, currStyles.box]}>
+        <View
+          style={[
+            currStyles.label,
+            // eslint-disable-next-line react-native/no-inline-styles
+            {
+              backgroundColor: debt.saldo > 0 ? '#F80012' : '#00C322',
+            },
+          ]}
+        />
+        <View style={currStyles.info}>
+          <SubTitle style={styles.title}>{`Договор №${contact?.contractNumber} от ${contact?.contractDate}`}</SubTitle>
+          {contact && (
+            <>
+              <Text>{`Условия оплаты: ${contact.paycond}`}</Text>
+              <Text>{`Задолженность: ${debt.saldo}`}</Text>
+            </>
+          )}
+        </View>
+      </View>
+      {process ? (
+        <ActivityIndicator size="large" color="#3914AF" />
+      ) : visits.length > 0 ? (
+        <>
+          <Visit item={visits[0]} />
+          {/*<SubTitle style={styles.title}>Визиты</SubTitle>
+          <Divider />
+          <FlatList
+            ref={ref}
+            data={visits}
+            keyExtractor={(_, i) => String(i)}
+            renderItem={renderItem}
+            scrollEventThrottle={400}
+            ItemSeparatorComponent={ItemSeparator}
+          />*/}
+        </>
+      ) : (
+        <Button onPress={handleNewVisit} mode="contained" style={[globalStyles.rectangularButton, currStyles.buttons]}>
+          Начать визит
+        </Button>
+      )}
     </View>
   );
 };
 
 export default RouteDetailScreen;
+
+const currStyles = StyleSheet.create({
+  box: {
+    borderColor: '#8888',
+    borderRadius: 10,
+    borderWidth: 0.5,
+    marginBottom: 10,
+  },
+  buttons: {
+    width: '100%',
+  },
+  content: {
+    justifyContent: 'flex-start',
+  },
+  info: {
+    margin: 5,
+    marginLeft: 15,
+  },
+  label: {
+    width: 10,
+    backgroundColor: '#3914AF',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+});
