bf1542a500ed3638b0d9fa6e69a43ac156451294
Stanislau Shliakhtsich <stasgm@gmail.com>
2021-06-04 17:06:12 +0300

    feat: info component

---
+++ apps/app-trade-agents/src/screens/Orders/components/Info.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { globalStyles as styles, SubTitle } from '@lib/mobile-ui';
+
+interface IProps {
+  colorLabel: string;
+  title: string;
+  children: React.ReactElement;
+}
+
+const Info = ({ colorLabel, title, children }: IProps) => {
+  return (
+    <View style={[styles.flexDirectionRow, currStyles.box]}>
+      <View style={[currStyles.label, { backgroundColor: colorLabel }]} />
+      <View style={currStyles.info}>
+        <SubTitle style={[styles.title]}>{title}</SubTitle>
+        {children}
+      </View>
+    </View>
+  );
+};
+
+export default Info;
+
+const currStyles = StyleSheet.create({
+  box: {
+    borderColor: '#8888',
+    borderRadius: 10,
+    borderWidth: 0.5,
+    marginBottom: 10,
+  },
+  info: {
+    margin: 5,
+    marginLeft: 15,
+  },
+  label: {
+    width: 10,
+    backgroundColor: '#3914AF',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+});
--- apps/app-trade-agents/src/screens/Routes/RouteDetailScreen.tsx
+++ apps/app-trade-agents/src/screens/Routes/RouteDetailScreen.tsx
@@ -1,13 +1,12 @@
-import { SubTitle } from '@lib/mobile-ui/src/components';
-import { docSelectors } from '@lib/store';
-import { RouteProp, useRoute } from '@react-navigation/native';
 import React, { useState } from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { ActivityIndicator, Text, View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
-
 import * as Location from 'expo-location';
 
-import { globalStyles, globalStyles as styles } from '@lib/mobile-ui';
+import { docSelectors } from '@lib/store';
+
+import { SubTitle, globalStyles as styles } from '@lib/mobile-ui';
 
 import { INamedEntity, IReference } from '@lib/types';
 
@@ -42,7 +41,7 @@ const RouteDetailScreen = () => {
 
   const [process, setProcess] = useState(false);
 
-  const point = ((docSelectors.selectByDocType('route') as unknown) as IRouteDocument[])
+  const point = (docSelectors.selectByDocType('route') as unknown as IRouteDocument[])
     ?.find((e) => e.id === routeId)
     ?.lines.find((i) => i.id === id);
 
@@ -58,9 +57,9 @@ const RouteDetailScreen = () => {
   /*const outlet = point
     ? (refSelectors.selectByName('outlet') as IReference<IOutlet>)?.data?.find((e) => e.id === point.outlet.id)
     : undefined;*/
-  const outlet = ((outletRefMock as unknown) as IReference<IOutlet>).data?.find((item) => item.id === point.outlet.id);
+  const outlet = (outletRefMock as unknown as IReference<IOutlet>).data?.find((item) => item.id === point.outlet.id);
   const contact = outlet
-    ? ((contactRefMock as unknown) as IReference<IContact>).data?.find((item) => item.id === outlet?.company.id)
+    ? (contactRefMock as unknown as IReference<IContact>).data?.find((item) => item.id === outlet?.company.id)
     : undefined;
 
   const debt: IDebt = {
@@ -89,7 +88,6 @@ const RouteDetailScreen = () => {
         id: `${id}${date}`,
         routeLineId: Number(id),
         dateBegin: date,
-        // beginGeoPoint: { latitude: 53.89076, longitude: 27.551006 } as ICoords,
         beginGeoPoint: coords as unknown as ICoords,
         takenType: 'ONPLACE',
       }),
@@ -112,7 +110,8 @@ const RouteDetailScreen = () => {
       </Info>
       <Info
         colorLabel={debt.saldo > 0 ? '#F80012' : '#00C322'}
-        title={`Договор №${contact?.contractNumber} от ${contact?.contractDate}`}>
+        title={`Договор №${contact?.contractNumber} от ${contact?.contractDate}`}
+      >
         <>
           {contact && (
             <>
@@ -137,7 +136,7 @@ const RouteDetailScreen = () => {
           ))}
         </>
       ) : (
-        <Button onPress={handleNewVisit} mode="contained" style={[globalStyles.rectangularButton, currStyles.buttons]}>
+        <Button onPress={handleNewVisit} mode="contained" style={[styles.rectangularButton, currStyles.buttons]}>
           Начать визит
         </Button>
       )}
