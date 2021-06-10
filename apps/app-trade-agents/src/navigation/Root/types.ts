921800c079db5b5cb091af500149aa31b48db4a9
Elena Buraya <lenalatkovich@gmail.com>
2021-06-10 18:21:44 +0300

    fix: navigator in routes

--- apps/app-trade-agents/src/navigation/Root/types.ts
+++ apps/app-trade-agents/src/navigation/Root/types.ts
@@ -22,6 +22,8 @@ export type RoutesStackParamList = {
   RouteDetails: { routeId: string; id: string };
   OrderView: { id: string } | undefined;
   OrderLine: { mode: number; docId: string; item: IOrderLine };
+  SelectGroupItem: { docId: string };
+  SelectGoodItem: { docId: string; groupId: string };
   SelectItem: { docId: string; name: string };
   ReturnView: { id: string } | undefined;
   ReturnLine: { mode: number; docId: string; item: IReturnLine };
