791b531685cd2c72030f574ac86b8e9e2e627386
Elena Buraya <lenalatkovich@gmail.com>
2021-06-14 12:30:58 +0300

    fix: open

--- apps/app-trade-agents/src/screens/Orders/SelectGroupScreen.tsx
+++ apps/app-trade-agents/src/screens/Orders/SelectGroupScreen.tsx
@@ -40,8 +40,7 @@ const Group = ({
 
   const nextLevelGroups = groups.data.filter((group) => group.parent?.id === item.id);
 
-  const isExpand =
-    !!expendGroup || expendGroup === item.id || !!nextLevelGroups.find((group) => group.id === expendGroup);
+  const isExpand = expendGroup === item.id || !!nextLevelGroups.find((group) => group.id === expendGroup);
 
   const icon = (nextLevelGroups.length === 0 ? 'chevron-right' : isExpand ? 'chevron-up' : 'chevron-down') as Icon;
 
