ed8a455af3b54eed8d9c2e0157394ccf2662d962
Kostyanaya <kostyanaya.yuliya@gmail.com>
2021-07-26 19:31:55 +0300

    chore: edit DeviceDetails

--- apps/web-admin/src/components/device/DeviceDetails.tsx
+++ apps/web-admin/src/components/device/DeviceDetails.tsx
@@ -1,8 +1,14 @@
 import { Box, Card, CardContent, Grid, TextField, Divider, Button } from '@material-ui/core';
 
-import { IDevice } from '@lib/types';
-import { useFormik } from 'formik';
+import { IDevice, INamedEntity } from '@lib/types';
+import { useFormik, Field } from 'formik';
 import * as yup from 'yup';
+
+import { useEffect, useState } from 'react';
+
+import ComboBox from '../ComboBox';
+
+import { deviceStates } from '../../utils/constants';
 
 interface IProps {
   loading: boolean;
@@ -11,17 +17,51 @@ interface IProps {
   onCancel: () => void;
 }
 
+// export interface IDeviceBindingFormik extends Omit<IDeviceBinding, 'state'> {
+//   state: INamedEntity;
+// }
+
 const DeviceDetails = ({ device, loading, onSubmit, onCancel }: IProps) => {
+
+  // const initialValues: IDeviceFormik = {
+  //   ...deviceBinding,
+  //   user: deviceBinding.user || null,
+  //   device: deviceBinding.device || null,
+  //   state: { id: deviceBinding.state, name: deviceStates[deviceBinding.state] },
+  // };
+
   const formik = useFormik<IDevice>({
     enableReinitialize: true,
     initialValues: device,
     validationSchema: yup.object().shape({
       name: yup.string().required('Required'),
+      state: yup.string().required('Required'),
     }),
     onSubmit: (values) => {
-      onSubmit(values);
+      onSubmit(/*{*/ values /*, state: values.state /*.id*/ /*} as IDevice*/);
     },
   });
+
+  // useEffect(() => {
+  //   let unmounted = false;
+  //   const getDevices = async () => {
+  //     const res = await api.device.getDevices();
+  //     if (res.type === 'GET_DEVICES' && !unmounted) {
+  //       setDevices(res.devices.map((d) => ({ id: d.id, name: d.name })));
+  //       setLoadingDevices(false);
+  //     }
+  //   };
+  //   getDevices();
+  //   return () => {
+  //     unmounted = true;
+  //   };
+  // }, []);
+
+  // const [devices, setDevices] = useState<INamedEntity[]>([]);
+
+  // const initialValues: IDeviceBindingFormik = {
+  //       state: { id: deviceBinding.state, name: deviceStates[deviceBinding.state] },
+  // };
 
   return (
     <>
@@ -48,6 +88,36 @@ const DeviceDetails = ({ device, loading, onSubmit, onCancel }: IProps) => {
                     type="name"
                     disabled={loading}
                     value={formik.values.name}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    /*<Field*/
+                    component={ComboBox}
+                    name="state"
+                    label="Статус"
+                    type="state"
+                    options={Object.entries(deviceStates).map((key) => ({ id: key[0], name: key[1] })) || []}
+                    setFieldValue={formik.setFieldValue}
+                    setTouched={formik.setTouched}
+                    error={Boolean(formik.touched.state && formik.errors.state)}
+                    disabled={loading}
+
+                    //error={formik.touched.state && Boolean(formik.errors.state)}
+                    //  fullWidth
+                    // name="state"
+                    // label="Статус"
+
+                    //required
+                    //variant="outlined"
+                    //options={devices?.map((d) => ({ id: d.id, state: d.state })) || []}
+                    //onBlur={formik.handleBlur}
+                    //onChange={formik.handleChange}
+                    //   type="state"
+                    //   disabled={loading}
+                    //  // value={formik.values.state}
+                    //   setFieldValue={formik.setFieldValue}
+                    //   error={Boolean(formik.touched.state && formik.errors.state)}
                   />
                 </Grid>
               </Grid>
