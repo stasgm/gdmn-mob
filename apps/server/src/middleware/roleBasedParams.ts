011f6354c1e64904e2b0d751e87e5489156ff596
Kostyanaya <kostyanaya.yuliya@gmail.com>
2021-08-04 17:57:26 +0300

    chore: device service

--- apps/server/src/middleware/roleBasedParams.ts
+++ apps/server/src/middleware/roleBasedParams.ts
@@ -9,6 +9,14 @@ export const roleBasedParamsMiddlware = async (ctx: Context, next: Next): Promis
     // const { companyId } = ctx.request.query;
 
     // if (!companyId) {
+    // ctx.request.query.companyId = user.company?.id;
+
+    if (user?.role == 'Admin') {
+      // console.log('1234');
+      // console.log('1235', user);
+      //user?.role != 'SuperAdmin';
+      //ctx.request.query.user != 'SuperAdmin';
+    }
     ctx.request.query.companyId = user.company?.id;
     // }
   }
--- apps/server/src/services/deviceService.ts
+++ apps/server/src/services/deviceService.ts
@@ -1,4 +1,4 @@
-import { IDBDevice, IDevice, INamedEntity, NewDevice } from '@lib/types';
+import { IDBDevice, IDevice, INamedEntity, NewDevice, IDBActivationCode, IActivationCode } from '@lib/types';
 
 import { ConflictException, DataNotFoundException } from '../exceptions';
 
@@ -85,12 +85,16 @@ const updateOne = async (id: string, deviceData: Partial<IDevice>, params?: Reco
  * */
 const deleteOne = async ({ deviceId }: { deviceId: string }): Promise<void> => {
   const { devices } = getDb();
+  const { codes } = getDb();
+  const { deviceBindings } = getDb();
 
   if (!(await devices.find((device) => device.id === deviceId))) {
     throw new DataNotFoundException('Устройство не найдено');
   }
 
   await devices.delete((device) => device.id === deviceId);
+  await deviceBindings.delete((deviceBinding) => deviceBinding.deviceId === deviceId);
+  await codes.delete((activationCode) => activationCode.deviceId === deviceId);
 };
 
 const findOne = async (id: string): Promise<IDevice | undefined> => {
