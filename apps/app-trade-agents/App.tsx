4f5cd8388914209b4ebdfe653f728945df5ebcb1
sunnycreature <sunnycreature@gmail.com>
2021-08-26 12:19:05 +0300

    chore: persistor

--- apps/app-trade-agents/App.tsx
+++ apps/app-trade-agents/App.tsx
@@ -1,31 +1,114 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import { MobileApp } from '@lib/mobile-app';
+import { INavItem } from '@lib/mobile-navigation';
 
-import { authSelectors } from '@lib/store';
-import { AuthNavigator } from '@lib/mobile-auth';
-import { Theme as defaultTheme, Provider as UIProvider } from '@lib/mobile-ui';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import RootNavigator from './src/navigation/RootNavigator';
+import { persistor, store } from './src/store';
 
-import { store } from './src/store';
+import RoutesNavigator from './src/navigation/Root/RoutesNavigator';
+import OrdersNavigator from './src/navigation/Root/OrdersNavigator';
+import ReturnsNavigator from './src/navigation/Root/ReturnsNavigator';
+import MapNavigator from './src/navigation/Root/Maps/MapNavigator';
 
-const App = () => {
-  const Router = () => (authSelectors.isLoggedWithCompany() ? <RootNavigator /> : <AuthNavigator />);
-  // const Router = () => <RootNavigator />;
+const Root = () => {
+  const navItems: INavItem[] = [
+    /*   {
+      name: 'Dashboard',
+      title: 'Дашборд',
+      icon: 'view-dashboard-outline',
+      component: DashboardNavigator,
+    }, */
+    {
+      name: 'Routes',
+      title: 'Маршруты',
+      icon: 'routes',
+      component: RoutesNavigator,
+    },
+    {
+      name: 'Orders',
+      title: 'Заявки',
+      icon: 'clipboard-list-outline',
+      component: OrdersNavigator,
+    },
+    {
+      name: 'Returns',
+      title: 'Возвраты',
+      icon: 'file-restore',
+      component: ReturnsNavigator,
+    },
+    {
+      name: 'Map',
+      title: 'Карта',
+      icon: 'map-outline',
+      component: MapNavigator,
+    },
+  ];
 
   return (
-    <Provider store={store}>
-      <UIProvider theme={defaultTheme}>
-        <ActionSheetProvider>
-          <NavigationContainer>
-            <Router />
-          </NavigationContainer>
-        </ActionSheetProvider>
-      </UIProvider>
-    </Provider>
+    // <MobileApp store={store} items={navItems} /> - если не нужен доступ к Store извне
+    <MobileApp items={navItems} />
   );
 };
 
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Root />
+    </PersistGate>
+  </Provider>
+);
+
 export default App;
+
+// const App = () => {
+//   const navItems: INavItem[] = [
+//     /*   {
+//       name: 'Dashboard',
+//       title: 'Дашборд',
+//       icon: 'view-dashboard-outline',
+//       component: DashboardNavigator,
+//     }, */
+//     {
+//       name: 'Routes',
+//       title: 'Маршруты',
+//       icon: 'routes',
+//       component: RoutesNavigator,
+//     },
+//     {
+//       name: 'Orders',
+//       title: 'Заявки',
+//       icon: 'clipboard-list-outline',
+//       component: OrdersNavigator,
+//     },
+//     {
+//       name: 'Returns',
+//       title: 'Возвраты',
+//       icon: 'file-restore',
+//       component: ReturnsNavigator,
+//     },
+//     {
+//       name: 'Map',
+//       title: 'Карта',
+//       icon: 'map-outline',
+//       component: MapNavigator,
+//     },
+//   ];
+//   const Router = () => (authSelectors.isLoggedWithCompany() ? <RootNavigator /> : <AuthNavigator />);
+//   // const Router = () => <RootNavigator />;
+
+//   return (
+//     <Provider store={store}>
+//       <UIProvider theme={defaultTheme}>
+//         <ActionSheetProvider>
+//           <NavigationContainer>
+//             <Router />
+//           </NavigationContainer>
+//         </ActionSheetProvider>
+//       </UIProvider>
+//     </Provider>
+//   );
+// };
+
+// export default App;
--- apps/appl-request/App.tsx
+++ apps/appl-request/App.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { MobileApp } from '@lib/mobile-app';
 import { INavItem } from '@lib/mobile-navigation';
 import { PersistGate } from 'redux-persist/integration/react';
+
 import { persistor, store } from './src/store';
 import ApplNavigator from './src/navigation/Root/ApplNavigator';
 
@@ -20,31 +21,11 @@ const Root = () => {
     [],
   );
 
-  // const handleSync = async () => {
-  //   setLoading(true);
-
-  //   await dispatch(referenceActions.clearReferences());
-  //   await dispatch(documentActions.clearDocuments());
-  //   await dispatch(messageActions.clearMessages());
-
-  //   await dispatch(
-  //     messageActions.fetchMessages({
-  //       companyId: company!.id,
-  //       systemId: 'gdmn-appl-request',
-  //     }),
-  //   );
-
-  //   // dispatch(referenceActions.addReferences(applRefs));
-  //   // dispatch(documentActions.addDocuments(applDocuments));
-  //   dispatch(messageActions.addMessages(applMessages));
-
-  //   setLoading(false);
-  // };
-
   return (
     // <MobileApp store={store} items={navItems} /> - если не нужен доступ к Store извне
-    <MobileApp items={navItems}/>
-  )};
+    <MobileApp items={navItems} />
+  );
+};
 
 const App = () => (
   <Provider store={store}>
