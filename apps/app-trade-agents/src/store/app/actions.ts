b87b4fcd12a6b92e9142785b4f8f7bf955c3afaa
sunnycreature <sunnycreature@gmail.com>
2021-09-02 10:58:22 +0300

    chore: actions

--- apps/app-trade-agents/src/store/app/actions.ts
+++ apps/app-trade-agents/src/store/app/actions.ts
@@ -1,14 +1,5 @@
-import { IEntity, IDocument } from '@lib/types';
-import { ActionType, createAction, createAsyncAction } from 'typesafe-actions';
+import { ActionType } from 'typesafe-actions';
 
-const removeDocumentAsync = createAsyncAction(
-  'DOCUMENTS/REMOVE_DOCUMENT',
-  'DOCUMENTS/REMOVE_DOCUMENT_SUCCESS',
-  'DOCUMENTS/REMOVE_DOCUMENT_FAILURE',
-)<string | undefined, string, string>();
-
-export const appActions = {
-  removeDocumentAsync,
-};
+export const appActions = {};
 
 export type AppActionType = ActionType<typeof appActions>;
