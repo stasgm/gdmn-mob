2662d33319d29fa54fc499744cc77712dc5b491d
sunnycreature <sunnycreature@gmail.com>
2021-09-02 14:09:38 +0300

    fix: visit is document

--- apps/app-trade-agents/src/store/visits/types.ts
+++
@@ -1,9 +0,0 @@
-// import { IDocument, IEntity, IHead, MandateProps } from '@lib/types';
-
-// import { ICoords } from '../geo/types';
-
-// export type IVisitState = {
-//   readonly list: IVisitDocument[];
-//   readonly loading: boolean;
-//   readonly errorMessage: string;
-// };
