f3be77d6579e8db8b8853faec892f2e433fb467e
Kostyanaya <kostyanaya.yuliya@gmail.com>
2021-12-14 15:03:29 +0300

    chore: added unknown good

--- apps/app-inventory/src/screens/Inventory/components/Scanners/ScanBarcode.tsx
+++ apps/app-inventory/src/screens/Inventory/components/Scanners/ScanBarcode.tsx
@@ -10,7 +10,6 @@ import { useNavigation, useTheme, RouteProp, useRoute } from '@react-navigation/
 
 import styles from '@lib/mobile-ui/src/styles/global';
 import { scanStyle } from '@lib/mobile-ui/src/styles/scanStyle';
-import { ScanButton } from '@lib/mobile-ui';
 import { useSelector, docSelectors } from '@lib/store';
 
 import { INamedEntity, ISettingsOption } from '@lib/types';
@@ -59,10 +58,6 @@ export const ScanBarcodeScreen = () => {
     setBarcode(data);
   };
 
-  const handleScannerGood = useCallback(() => {
-    navigation.navigate('InventoryEdit');
-  }, [navigation]);
-
   useEffect(() => {
     vibroMode && Vibration.vibrate(oneSecund);
   }, [vibroMode]);
@@ -82,7 +77,10 @@ export const ScanBarcodeScreen = () => {
       let charTo = weightSettingsWeightCode.data.length;
 
       if (brc.substring(charFrom, charTo) !== weightSettingsWeightCode.data) {
-        const remItem = goods?.[Object.keys(goods).find((item) => goods[item].barcode === brc) || ''];
+        const remItem =
+          goods?.[
+            Object.keys(goods).find((item) => goods[item].barcode === brc || goods[item].id === 'unknown1') || ''
+          ];
 
         if (!remItem) {
           return;
@@ -94,9 +92,9 @@ export const ScanBarcodeScreen = () => {
           good: { id: good.id, name: good.name } as INamedEntity,
           id: uuid(),
           quantity: 1,
-          price: remains!.length ? remains![0].price : 0,
-          remains: remains!.length ? remains![0].q : 0,
-          barcode: good.barcode,
+          price: remains?.length ? remains[0].price : 0,
+          remains: remains?.length ? remains?.[0].q : 0,
+          barcode: good.id === 'unknown' ? barcode : good.barcode,
         };
       }
 
@@ -116,7 +114,7 @@ export const ScanBarcodeScreen = () => {
       }
 
       const { remains, ...good } = remItem;
-
+      console.log('good.barcode', good.barcode);
       return {
         good: { id: good.id, name: good.name } as INamedEntity,
         id: uuid(),
@@ -153,6 +151,8 @@ export const ScanBarcodeScreen = () => {
   if (hasPermission === false) {
     return <Text style={styles.title}>Нет доступа к камере</Text>;
   }
+
+  console.log('123', itemLine?.barcode);
 
   return (
     <View style={[scanStyle.content, { backgroundColor: colors.card }]}>
@@ -218,9 +218,12 @@ export const ScanBarcodeScreen = () => {
             <View style={scanStyle.buttonsContainer}>
               <TouchableOpacity
                 style={[scanStyle.buttons, { backgroundColor: '#FFCA00' }]}
-                onPress={() => setScanned(false)}
+                onPress={() => {
+                  setScanned(false);
+                  setItemLine(undefined);
+                }}
               >
-                <ScanButton onPress={handleScannerGood} />
+                <IconButton icon="barcode-scan" size={30} />
                 <Text style={scanStyle.text}>Пересканировать</Text>
               </TouchableOpacity>
             </View>
@@ -238,7 +241,10 @@ export const ScanBarcodeScreen = () => {
             {scanned && itemLine && (
               <View style={scanStyle.buttonsContainer}>
                 <TouchableOpacity
-                  style={[scanStyle.buttons, { backgroundColor: '#4380D3' }]}
+                  style={[
+                    scanStyle.buttons,
+                    { backgroundColor: itemLine.good.id === 'unknown' ? '#CC3C4D' : '#4380D3' },
+                  ]}
                   onPress={() => {
                     navigation.navigate('InventoryLine', {
                       mode: 0,
@@ -265,7 +271,7 @@ export const ScanBarcodeScreen = () => {
         {!scanned && (
           <View style={scanStyle.footer}>
             <>
-              <ScanButton onPress={handleScannerGood} />
+              <IconButton icon="barcode-scan" size={30} />
               <Text style={scanStyle.text}>Наведите рамку на штрихкод</Text>
             </>
           </View>
