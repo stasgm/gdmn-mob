46fe9667974f8c99edf829de96b5028db95fb7a7
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-01-18 16:26:00 +0300

    chore: update api config

--- apps/app-trade-agents/app.config.js
+++ apps/app-trade-agents/app.config.js
@@ -8,7 +8,6 @@ export default ({ config }) => ({
       ...config.android.config,
       googleMaps: {
         apiKey: process.env.GOOGLE_API_KEY,
-        // apiKey: 'AIzaSyBHC0awJaSoP-rQnctSsp6DmEYZ06SGTh0',
       },
     },
   },
