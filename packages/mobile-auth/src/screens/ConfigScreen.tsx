60c707b44496b83bdba4f177131cd4c40ebdf36f
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-04-15 14:51:20 +0300

    chore: add http check to config

--- packages/mobile-auth/src/screens/ConfigScreen.tsx
+++ packages/mobile-auth/src/screens/ConfigScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Platform, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import { HelperText } from 'react-native-paper';
 
 import { IApiConfig } from '@lib/client-types';
 import { AppScreen, Input, PrimeButton, ScreenTitle } from '@lib/mobile-ui';
@@ -28,7 +30,11 @@ const ConfigScreen = (props: Props) => {
   const [deviceId, setDeviceId] = useState(config?.deviceId || '');
 
   const handleSaveConfig = () => {
+    if (err) {
+      return;
+    }
     const match = serverName.match(/^(.*:\/\/)([A-Za-z0-9\-.]+)/);
+    console.log('match', match);
     const protocol: string = match?.[1] || '';
     const server: string = match?.[2] || '';
 
@@ -51,11 +57,35 @@ const ConfigScreen = (props: Props) => {
     navigation.navigate('Splash');
   };
 
+  const [err, setErr] = useState(false);
+
+  const hasErrors = () => {
+    if (!(serverName.includes('http://') || serverName.includes('https://'))) {
+      setErr(true);
+    } else {
+      setErr(false);
+    }
+    console.log('err1', err);
+    return;
+  };
+  console.log('err2', err);
+
   return (
     <AppScreen style={configStyles.screen}>
       <KeyboardAwareScrollView resetScrollToCoords={{ x: 0, y: 0 }} style={configStyles.keybord}>
         <ScreenTitle infoRow={false}>Настройка подключения</ScreenTitle>
-        <Input label="Адрес сервера" value={serverName} onChangeText={setServerName} clearInput={true} />
+        <Input
+          label="Адрес сервера"
+          value={serverName}
+          onChangeText={setServerName}
+          clearInput={true}
+          onEndEditing={hasErrors}
+        />
+        {err && (
+          <HelperText type="error" style={configStyles.error}>
+            {'Адрес сервера введен некорректно \nПример адреса: http://111.111.111.111'}
+          </HelperText>
+        )}
         <Input label="Порт" value={serverPort} onChangeText={setServerPort} clearInput={true} />
         <Input label="ID устройства" value={deviceId} onChangeText={setDeviceId} clearInput={true} />
         <View style={localStyles.buttonsView}>
@@ -86,4 +116,5 @@ export default ConfigScreen;
 const configStyles = StyleSheet.create({
   screen: { alignItems: 'center', flexDirection: 'row', flex: 1 },
   keybord: { paddingVertical: 20, flexDirection: 'column' },
+  error: { marginTop: -5, paddingTop: -5 },
 });
