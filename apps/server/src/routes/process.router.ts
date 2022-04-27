74da562bcd5d88c280da024562f4b611abd9581a
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-04-27 15:33:26 +0300

    chore: add new process middleware

--- apps/server/src/routes/process.router.ts
+++ apps/server/src/routes/process.router.ts
@@ -13,6 +13,7 @@ import {
 import { authMiddleware } from '../middleware/authRequired';
 import { deviceMiddleware } from '../middleware/deviceRequired';
 import { processParamsMiddlware } from '../middleware/processParams';
+import { getProcessParamsMiddlware } from '../middleware/getProcessParams';
 import { processValidation } from '../validations';
 
 const router = Router();
@@ -24,7 +25,14 @@ router.patch('/:id/prepare', processValidation.prepareProcess, authMiddleware, d
 router.delete('/:id/complete', processValidation.completeProcess, authMiddleware, deviceMiddleware, completeProcess);
 router.delete('/:id/cancel', processValidation.cancelProcess, authMiddleware, deviceMiddleware, cancelProcess);
 router.delete('/:id/interrupt', processValidation.interruptProcess, authMiddleware, deviceMiddleware, interruptProcess);
-router.get('/', processValidation.getProcesses, authMiddleware, deviceMiddleware, processParamsMiddlware, getProcesses);
+router.get(
+  '/',
+  processValidation.getProcesses,
+  authMiddleware,
+  deviceMiddleware,
+  getProcessParamsMiddlware,
+  getProcesses,
+);
 router.delete('/:id', processValidation.deleteProcess, authMiddleware, deviceMiddleware, deleteProcess);
 
 export default router;
