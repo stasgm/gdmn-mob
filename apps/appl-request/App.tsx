ab29252566867c2e2b82743b291e49f1fd7a6d56
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-05-27 13:38:11 +0300

    chore: appl-request mocks

--- apps/appl-request/App.tsx
+++ apps/appl-request/App.tsx
@@ -1,15 +1,31 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Provider } from 'react-redux';
 
 import { MobileApp } from '@lib/mobile-app';
 import { INavItem } from '@lib/mobile-navigation';
 
-import { appActions, appSelectors, authSelectors, useDispatch, useSelector } from '@lib/store';
+import {
+  appActions,
+  appSelectors,
+  authSelectors,
+  useDispatch,
+  useSelector,
+  useRefThunkDispatch,
+  useDocThunkDispatch,
+  referenceActions,
+  documentActions,
+} from '@lib/store';
 import { globalStyles as styles, Theme as defaultTheme, Provider as UIProvider, AppScreen } from '@lib/mobile-ui';
 import { ActivityIndicator, Caption, useTheme } from 'react-native-paper';
 
+import { IDocument, IReferences } from '@lib/types';
+
+import { sleep } from '@lib/client-api';
+
 import ApplNavigator from './src/navigation/Root/ApplNavigator';
 import { store } from './src/store';
+
+import { messageRequest, ONE_SECOND_IN_MS } from './src/utils/constants';
 
 const Root = () => {
   const navItems: INavItem[] = useMemo(
@@ -30,6 +46,20 @@ const Root = () => {
   const appDataLoading = appSelectors.selectLoading();
   const appLoading = useSelector((state) => state.app.loading);
   const isLogged = authSelectors.isLoggedWithCompany();
+  const isDemo = useSelector((state) => state.auth.isDemo);
+
+  const refDispatch = useRefThunkDispatch();
+  const docDispatch = useDocThunkDispatch();
+
+  const getMessages = useCallback(async () => {
+    await sleep(ONE_SECOND_IN_MS);
+    await refDispatch(
+      referenceActions.setReferences(messageRequest.find((m) => m.body.type === 'REFS')?.body.payload as IReferences),
+    );
+    await docDispatch(
+      documentActions.setDocuments(messageRequest.find((m) => m.body.type === 'DOCS')?.body.payload as IDocument[]),
+    );
+  }, [refDispatch, docDispatch]);
 
   useEffect(() => {
     // console.log('useEffect loadGlobalDataFromDisc');
@@ -64,7 +94,7 @@ const Root = () => {
       </Caption>
     </AppScreen>
   ) : (
-    <MobileApp items={navItems} />
+    <MobileApp items={navItems} onGetMessages={isDemo ? getMessages : undefined} />
   );
 };
 
