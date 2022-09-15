b7f3e6a5b958d5af3bf9ee5c18c7d8ea6f8c2232
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-09-15 12:41:54 +0300

    chore: old package

--- apps/app-trade-agents/src/screens/Orders/components/OrderLine.tsx
+++ apps/app-trade-agents/src/screens/Orders/components/OrderLine.tsx
@@ -1,3 +1,4 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { styles } from '@lib/mobile-navigation';
 import { ItemSeparator } from '@lib/mobile-ui';
 import { refSelectors } from '@lib/store';
@@ -5,6 +6,7 @@ import { INamedEntity } from '@lib/types';
 
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { ScrollView, StyleSheet, TextInput, View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useTheme } from '@react-navigation/native';
 
 import { IOrderLine, IPackageGood } from '../../../store/types';
@@ -16,6 +18,8 @@ interface IProps {
   onSetLine: (value: IOrderLine) => void;
 }
 
+type Icon = keyof typeof MaterialCommunityIcons.glyphMap;
+
 const OrderLine = ({ item, onSetLine }: IProps) => {
   const { colors } = useTheme();
 
@@ -23,13 +27,11 @@ const OrderLine = ({ item, onSetLine }: IProps) => {
     .selectByName<IPackageGood>('packageGood')
     ?.data?.filter((e) => e.good.id === item.good.id);
 
-  const defaultPack = useMemo(
-    () => (packages.length === 1 ? packages[0].package : packages.find((i) => i.isDefault)?.package),
-    [packages],
-  );
+  const defaultPack = useMemo(() => packages.find((i) => i.isDefault)?.package, [packages]);
 
   const [goodQty, setGoodQty] = useState<string>(item?.quantity.toString());
   const [pack, setPack] = useState<INamedEntity | undefined>(item?.package || defaultPack);
+  const [isVisiblePackages, setIsVisiblePackages] = useState<boolean>(false);
 
   const qtyRef = useRef<TextInput>(null);
 
@@ -61,6 +63,7 @@ const OrderLine = ({ item, onSetLine }: IProps) => {
   }, [pack]);
 
   const textStyle = useMemo(() => [styles.number, styles.field, { color: colors.text }], [colors.text]);
+  const textPackStyle = useMemo(() => [localStyles.text, { color: colors.text }], [colors.text]);
 
   return (
     <>
@@ -96,25 +99,46 @@ const OrderLine = ({ item, onSetLine }: IProps) => {
             </View>
           </View>
           <ItemSeparator />
-          <View style={styles.item}>
-            <View style={styles.details}>
-              <Text style={styles.name}>Упаковка</Text>
-              {packages.length > 0 ? (
-                <View style={localStyles.packages}>
-                  {packages.map((elem) => (
-                    <Checkbox
-                      key={elem.package.id}
-                      title={elem.package.name}
-                      selected={elem.package.id === pack?.id}
-                      onSelect={() => setPack(elem.package.id === pack?.id ? undefined : elem.package)}
-                    />
-                  ))}
+          {packages.length > 0 ? (
+            <>
+              <TouchableOpacity style={styles.item} onPress={() => setIsVisiblePackages(!isVisiblePackages)}>
+                <View style={styles.details}>
+                  <Text style={styles.name}>Упаковка</Text>
+                  <Text style={textStyle}>{pack ? pack.name || 'упаковка не найдена' : ''}</Text>
                 </View>
-              ) : (
-                <Text style={textStyle}>Для данного товара нет упаковки</Text>
-              )}
-            </View>
-          </View>
+                <MaterialCommunityIcons
+                  name={(isVisiblePackages ? 'chevron-up' : 'chevron-down') as Icon}
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+              <View>
+                {isVisiblePackages && (
+                  <View style={localStyles.packages}>
+                    {packages.map((elem) => (
+                      <Checkbox
+                        key={elem.package.id}
+                        title={elem.package.name}
+                        selected={elem.package.id === pack?.id}
+                        onSelect={() => setPack(elem.package.id === pack?.id ? undefined : elem.package)}
+                      />
+                    ))}
+                  </View>
+                )}
+              </View>
+            </>
+          ) : (
+            <>
+              <View style={localStyles.item}>
+                <View style={localStyles.details}>
+                  <Text style={styles.name}>Упаковка</Text>
+                  <Text style={[styles.number, styles.field]}>{pack ? pack.name || 'упаковка не найдена' : ''}</Text>
+                </View>
+                <MaterialCommunityIcons name={'chevron-down' as Icon} size={24} color="black" />
+              </View>
+              <Text style={textPackStyle}>Для данного товара нет упаковки</Text>
+            </>
+          )}
           <ItemSeparator />
         </View>
       </ScrollView>
@@ -126,6 +150,21 @@ const localStyles = StyleSheet.create({
   packages: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+  },
+  text: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+  },
+  item: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 3,
+    marginTop: 3,
+  },
+  details: {
+    flex: 1,
+    marginHorizontal: 5,
+    marginTop: 5,
   },
 });
 
--- packages/mobile-ui/src/components/RadioGroup.tsx
+++ packages/mobile-ui/src/components/RadioGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { IListItem } from '@lib/mobile-types';
@@ -12,12 +12,15 @@ type Props = {
 };
 
 const RadioGroup = ({ options, onChange, activeButtonId, directionRow }: Props) => {
-  const onPress = (option: IListItem) => {
-    if (option.id === activeButtonId) {
-      return;
-    }
-    onChange(option);
-  };
+  const onPress = useCallback(
+    (option: IListItem) => {
+      if (option.id === activeButtonId) {
+        return;
+      }
+      onChange(option);
+    },
+    [onChange, activeButtonId],
+  );
 
   return (
     <View
@@ -31,7 +34,7 @@ const RadioGroup = ({ options, onChange, activeButtonId, directionRow }: Props) 
           <TouchableHighlight
             activeOpacity={0.4}
             key={option.id}
-            style={[localStyles.item]}
+            style={localStyles.item}
             underlayColor="#DDDDDD"
             onPress={() => onPress(option)}
           >
