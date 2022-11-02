5f24a48292aa7cdb455f2ad8707e8f0a072d68c6
Kostyanaya <kostyanaya.yuliya@gmail.com>
2022-11-02 11:35:23 +0300

    chore: screenState "idle" in docEdit

--- apps/app-gd-movement/src/screens/Doc/DocEditScreen.tsx
+++ apps/app-gd-movement/src/screens/Doc/DocEditScreen.tsx
@@ -188,6 +188,7 @@ export const DocEditScreen = () => {
         };
 
         dispatch(documentActions.updateDocument({ docId: id, document: updatedDoc }));
+        setScreenState('idle');
         navigation.navigate('DocView', { id });
       }
     }
