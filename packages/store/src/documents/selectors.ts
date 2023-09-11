8d9adca28c859bc4b224bbcaa55612950756c849
sunnycreature <sunnycreature@gmail.com>
2023-09-11 13:33:09 +0300

    chore: update orange, selectByDocType

--- packages/store/src/documents/selectors.ts
+++ packages/store/src/documents/selectors.ts
@@ -3,11 +3,11 @@ import { IDocument } from '@lib/types';
 import { useProxySelector } from '../';
 
 const selectByDocId = <T extends IDocument>(docId?: string) =>
-  useProxySelector<T>((state) => state.documents?.list.find((doc: T) => doc.id === docId), [docId]);
+  useProxySelector<T | undefined>((state) => state.documents?.list.find((doc: T) => doc.id === docId), [docId]);
 
 const selectByDocType = <T extends IDocument>(docType: string) =>
   useProxySelector<T[]>(
-    (state) => state.documents?.list.find((doc: T) => doc.documentType?.name === docType),
+    (state) => state.documents?.list.filter((doc: T) => doc.documentType?.name === docType) || [],
     [docType],
   );
 
