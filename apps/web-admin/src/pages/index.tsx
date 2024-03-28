8abc04a889e6ff4d6e3f0ddf6777081c28385f86
sunnycreature <sunnycreature@gmail.com>
2024-03-29 02:16:34 +0300

    chore: routes in admin

---
+++ apps/web-admin/src/pages/index.tsx
@@ -0,0 +1,14 @@
+export { default as AppSystems } from './AppSystems';
+export { default as Companies } from './Companies';
+export { default as DeviceLogs } from './DeviceLogs';
+export { default as Devices } from './Devices';
+export { default as Files } from './Files';
+export { default as Processes } from './Processes';
+export { default as ServerLogs } from './ServerLogs';
+export { default as Users } from './Users';
+
+export { default as Account } from './Account';
+export { default as Dashboard } from './Dashboard';
+export { default as Login } from './Login';
+export { default as NotFound } from './NotFound';
+export { default as Register } from './Register';
